d022eae759d4b356b2a7af52771f529bb3f234d4
Ronald Macmaster <ronnymacmaster@gmail.com>
2016-09-09 17:07:04 -0500

    lab2_report outline

--- lab2/adcswtrigger.d
+++ lab2/adcswtrigger.d
@@ -1,3 +1,3 @@
 .\adcswtrigger.o: ADCSWTrigger.c
-.\adcswtrigger.o: C:\Users\ronny\Desktop\EE\445L\keil\ARM\ARMCC\bin\..\include\stdint.h
+.\adcswtrigger.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
 .\adcswtrigger.o: tm4c123gh6pm.h
